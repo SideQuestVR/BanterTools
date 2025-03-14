ebcb9955b5fb40a4b34416e262fdecb2f7a33a3e
shane <shane@sidequestvr.com>
2025-03-14 10:53:12 +0100

    maybe leaderboard now stores the scores?

--- LeaderBoards/leader-boards.js
+++ LeaderBoards/leader-boards.js
@@ -69,7 +69,7 @@ class LeaderBoardsServer{
   async populateRoom(name) {
     const scores = [];
     // console.log(`banter-leaderboard:${name}:*`,'0');
-    const scan = await this.db.hScan(`banter-leaderboard:${name}:*`,'0');
+    const scan = await this.db.hScan(``,'0',{MATCH: `banter-leaderboard:${name}:*`});
     console.log(scan);
     // this.db.hScanIterator(`banter-leaderboard:${name}:*`).then(async (it) => {
     //   console.log(it);
