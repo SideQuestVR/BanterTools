fa8dc21a10f574c3b9f34ca237763df9f772e2a7
shane <shane@sidequestvr.com>
2025-03-16 09:03:48 +0100

    basic databse example

--- Markit/markit.js
+++ Markit/markit.js
@@ -1,6 +1,9 @@
 const { Client } = require('pg');
 const fs = require('fs');
 const path = require('path');
+const cors = require("cors");
+const https = require('https');
+
 (async()=>{
     const pass = fs.readFileSync(path.join(__dirname,'db.txt'), 'utf8');
     const db = new Client({
@@ -10,7 +13,23 @@ const path = require('path');
         port: 5432,
         database: 'markit',
     })
-    await db.connect()
+    await db.connect();
+
+    const app = express();
+    const privateKey = null;
+    const certificate = null;
+     
+    privateKey = fs.readFileSync( '../LegacyVideoPlayer/ssl/sdq.st.key' );
+    certificate = fs.readFileSync( '../LegacyVideoPlayer/ssl/sdq.st.cert' );
+    
+    const server = https.createServer({key: privateKey,cert: certificate}, app);
+    app.use(cors());
+
+    // app.use(express.static(path.join(__dirname, '..', 'public')));
+
+    server.listen( dohttp?80:443, function listening(){
+        console.log("Video Player started."); 
+    });
     
     try {
         const res = await db.query('SELECT $1::text as message', ['Hello world!'])
