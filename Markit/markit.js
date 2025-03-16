0f1a69535dd7f636f68eaf4fafa4834369949bea
shane <shane@sidequestvr.com>
2025-03-16 08:55:09 +0100

    basic databse example

--- Markit/markit.js
+++ Markit/markit.js
@@ -5,7 +5,7 @@ const path = require('path');
     const pass = fs.readFileSync(path.join(__dirname,'db.txt'), 'utf8');
     console.log(pass.substring(0,4));
     const db = new Client({
-        user: 'postgres',
+        user: 'markit',
         password: pass,
         host: 'localhost',
         port: 5432,
