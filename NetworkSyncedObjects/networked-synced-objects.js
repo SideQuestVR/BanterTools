d8b004184df9666ebaabecb3654eec849d92ba18
shane <shane@sidequestvr.com>
2025-04-10 14:46:36 +0200

    removed extra not the owner check

--- NetworkSyncedObjects/networked-synced-objects.js
+++ NetworkSyncedObjects/networked-synced-objects.js
@@ -268,7 +268,7 @@ class GameServer{
         });
         
         // After you take ownership, force an update to the clients.
-        this.tick();
+        // this.tick();
         break;
     }
   }
